274e41a7fee70c7256f1847da36f94c57b895264
Jeffrey Ruder and Krystan Menne <ruderjt@gmail.com>
2016-02-08 09:22:57 -0800

    add methods to determine triangle type

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,39 @@ function Triangle(sideOne, sideTwo, sideThree) {
 
 Triangle.prototype.sortedSides = function() {
   return [this.sideOne, this.sideTwo, this.sideThree].sort();
-}
+};
 
 Triangle.prototype.isTriangle = function() {
   return this.sortedSides()[0] + this.sortedSides()[1] > this.sortedSides()[2];
-}
+};
+
+Triangle.prototype.isEquilateral = function() {
+  return this.sortedSides()[0] === this.sortedSides()[2];
+};
+
+Triangle.prototype.isIsosceles = function() {
+  return (this.sortedSides()[0] === this.sortedSides()[1] ||
+    this.sortedSides()[1] === this.sortedSides()[2]) &&
+    !this.isEquilateral();
+};
+
+Triangle.prototype.isScalene = function () {
+  return this.sortedSides()[0] !== this.sortedSides()[1] &&
+    this.sortedSides()[1] !== this.sortedSides()[2];
+};
+
+Triangle.prototype.typeOfTriangle = function () {
+  if (!this.isTriangle()) {
+    return 'not a triangle';
+  } else if (this.isEquilateral()) {
+    return 'equilateral';
+  } else if (this.isIsosceles()) {
+    return 'isosceles';
+  } else {
+    return 'scalene';
+  }
+};
+
+$(function() {
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -8,10 +8,58 @@ describe ('Triangle', function() {
 
   it('knows if it is a valid triangle', function() {
     var testTriangle = createTestTriangle();
+    var notTriangle = createNonTriangle();
     expect(testTriangle.isTriangle()).to.equal(true);
+    expect(notTriangle.isTriangle()).to.equal(false);
+  });
+
+  it('knows if it is equilateral', function() {
+    var equilateralTriangle = createEquilateralTriangle();
+    var notEquilateral = createIsoscelesTriangle();
+    expect(equilateralTriangle.isEquilateral()).to.equal(true);
+    expect(notEquilateral.isEquilateral()).to.equal(false);
+  });
+
+  it('knows if it is isosceles', function() {
+    var isoscelesTriangle = createIsoscelesTriangle();
+    var notIsosceles = createEquilateralTriangle();
+    expect(isoscelesTriangle.isIsosceles()).to.equal(true);
+    expect(notIsosceles.isIsosceles()).to.equal(false);
+  });
+
+  it('knows if it is scalene', function() {
+    var scaleneTriangle = createScaleneTriangle();
+    var notScalene = createIsoscelesTriangle();
+    var notScaleneEquilateral = createEquilateralTriangle();
+    expect(scaleneTriangle.isScalene()).to.equal(true);
+    expect(notScalene.isScalene()).to.equal(false);
+    expect(notScaleneEquilateral.isScalene()).to.equal(false);
+  });
+
+  it('tells you what type of triangle it is', function() {
+    expect(createNonTriangle().typeOfTriangle()).to.equal('not a triangle');
+    expect(createEquilateralTriangle().typeOfTriangle()).to.equal('equilateral');
+    expect(createIsoscelesTriangle().typeOfTriangle()).to.equal('isosceles');
+    expect(createScaleneTriangle().typeOfTriangle()).to.equal('scalene');
   });
 });
 
 var createTestTriangle = function() {
   return new Triangle(3, 4, 5);
-}
+};
+
+var createNonTriangle = function() {
+  return new Triangle(2, 2, 5);
+};
+
+var createEquilateralTriangle = function() {
+  return new Triangle(3, 3, 3);
+};
+
+var createIsoscelesTriangle = function() {
+ return new Triangle(2, 2, 3);
+};
+
+var createScaleneTriangle = function() {
+  return new Triangle(3, 4, 5);
+};
